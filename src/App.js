acea996b74a8ddd0c4debb25192936f345ff10cd
tlewandowski18 <tlewandowski18@gmail.com>
2020-01-26 21:13:38 -0500

    create character list and routes. add styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList"
+import Navigation from "./components/Navigation"
+import WelcomePage from "./components/WelcomePage"
+import { Route } from "react-router-dom"
+import "./App.css"
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Navigation />
+      <Route exact path = "/" component={WelcomePage} />
+      <Route path = "/characters" component= {CharacterList} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,51 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
+
+  const CharacterBox = styled.div `
+    width: 90%;
+    margin: 2% auto;
+    border: 2px solid blue;
+    padding: 1% 3%;
+    background-color: beige;
+    border-radius: 20px;
+    
+  `
+  const Name = styled.h1 `
+  
+  `
+
+  
+
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(res => {
+        setCharacters(res.data.results)
+      })
+      .catch(err => {
+        console.log("You have an error! ", err)
+      })
   }, []);
 
+  console.log(characters)
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => {
+        return (
+          <CharacterBox key={character.id}>
+            <h1>{character.name}</h1>
+            <h2>Species: {character.species}</h2>
+            <h2>Status: {character.status}</h2>
+            <p>Number of Episodes: {character.episode.length}</p>
+          </CharacterBox>
+        )
+      })}
     </section>
   );
 }
